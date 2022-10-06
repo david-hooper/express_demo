b3e78e19086f89fb884b8ee105f2d4b5e314f14c
Jeff Haddock <jeff.haddock.developer@gmail.com>
2022-10-06 14:28:46 -0400

    created a front-end-server and added a UI folder for static view as well

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 const { Client } = require('pg');
+const res = require('express/lib/response');
 const config = require('./config.json')[process.env.NODE_ENV||"dev"]
+
 
 const connectionString = 'postgresql://postgres:docker@127.0.0.1:5432/studentdb';
 const client = new Client({
@@ -9,6 +12,9 @@ const client = new Client({
 client.connect();
 
 const app = express()
+app.use(cors());
+app.use(express.json());
+
 const PORT = 3000
 //console.log(process.env);
 //const HOST = '0.0.0.0'
@@ -44,7 +50,7 @@ app.get('/', (req, res) => {
 app.get('/api/students', (req, res) => {
     client.query('SELECT * FROM students')
     .then(result => {
-        console.log(result.rows[0])
+        // console.log(result.rows[0])
         res.send(result.rows);
     })
     .catch(e => console.error(e.stack))
@@ -66,6 +72,28 @@ app.get('/api/students/:id', (req, res) => {
     })
     .catch(e => console.error(e.stack))
 });
+
+app.post('/api/students/', (request, response) => {
+
+    let studentJson = request.body;
+    console.log(studentJson);
+    if(studentJson.age && studentJson.first_name){
+        client.query("INSERT INTO students (first_name, age) VALUES ($1, $2)"
+            , [studentJson.first_name, studentJson.age]
+            , (error, result) => {
+                if(error){
+                    response.status(500).send(error);
+                } else {
+                    console.log("response successful", result);
+                    response.status(201).send("Success!")
+                }
+            }
+        ); 
+    }
+    else {
+        response.status(500).send("you need to send an age and a name!!")
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Our app running on ${PORT}`)
